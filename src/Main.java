77364b5ae4888fa6c51fd9479800f7bddd81bcef
Andrey <andy523451@gmail.com>
2024-01-19 19:37:16 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Noviy god is coming!");
     }
 
 }
+//git init
+//git add .
+//git remote add origin https://github.com/Nitorie/hw1.git
+//git commit -m "add Hello World"
+//git push -u origin master
